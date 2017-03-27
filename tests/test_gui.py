038fb2db028a6ea4f4206f64159cf633354194e0
Hans E. Plesser <hans.ekkehard.plesser@nmbu.no>
2017-03-27 11:20:28 +0200

    Added copyright and license information.

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+#
+# test_gui.py
+#
+# This file is part of the NEST Connection App.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST Connection App is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST Connection App is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST Connection App.  If not, see <http://www.gnu.org/licenses/>.
+
 import unittest
 from matplotlib.backend_bases import MouseEvent
 from ..selector import PointsSelector, RectangleSelector, EllipseSelector
