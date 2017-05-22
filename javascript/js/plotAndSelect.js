f49dd51e7926940fb24502f52e906aa57256f271
Stine Vennemo <stine.brekke.vennemo@nmbu.no>
2017-05-22 17:04:50 +0200

    GUI - nothing works but framework is up

--- javascript/js/plotAndSelect.js
+++ javascript/js/plotAndSelect.js
@@ -16,10 +16,10 @@ var camera, scene, renderer, geometry, materials;
 var cameraControls;
 var effectController;
 
-var mouseup = true;
-var mousedown = false;
-var mousecoords;
-var mRelPos = {};
+var mouseUp = true;
+var mouseDown = false;
+var mouseCoords = { x: 0, y: 0 };
+var mRelPos = { x: 0, y: 0 };
 
 var nSelected = 0;
 
@@ -31,11 +31,12 @@ init();
 
 function init()
 {
-    container = document.createElement( 'div' );
+    //container = document.createElement( 'div' );
+    container = document.getElementById( 'main_body' );
 	document.body.appendChild( container );
 
     // CAMERA
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 8000 );
+	camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 1, 8000 );
 	camera.position.set( 0, 0, 2 );
 
 	scene = new THREE.Scene();
@@ -79,13 +80,12 @@ function init()
 
     points = new THREE.Points( geometry, material );
     
-    
     scene.add( points );
     
     // RENDERER
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize( container.clientWidth, container.clientHeight );
 
 	container.appendChild( renderer.domElement );
 	
@@ -101,7 +101,7 @@ function init()
 	window.addEventListener( 'resize', onWindowResize, false );
 	
 	//GUI
-	setUpGUI();
+	//setUpGUI();
 
     render();
 }
@@ -109,11 +109,12 @@ function init()
 // Selection
 function resetMarquee ()
 {
-  mouseup = true;
-  mousedown = false;
+  mouseUp = true;
+  mouseDown = false;
   marquee.fadeOut();
   marquee.css({width: 0, height: 0});
-  mousedowncoords = {};
+  mouseDownCoords.x = 0;
+  mouseDownCoords.y = 0;
 }
 
 // takes the mouse up and mouse down positions and calculates an origin
@@ -145,17 +146,11 @@ function findBounds (pos1, pos2)
     return ({lower_left: lower_left, upper_right: upper_right});
 }
 
-
 // Takes a position and detect if it is within delta of the origin defined by findBounds ({origin, delta})
 function withinBounds(pos, bounds)
 {
-
     var ll = bounds.lower_left;
     var ur = bounds.upper_right;
-
-    //console.log("withinBounds:");
-    //console.log(ll);
-    //console.log(ur);
 
     if ((pos.x >= ll.x) && (pos.x <= ur.x) && (pos.y >= ll.y) && (pos.y <= ur.y))
     {
@@ -171,35 +166,32 @@ function toScreenXY (point_pos) {
     projScreenMat.multiplyMatrices( camera.projectionMatrix, camera.matrixWorldInverse );
     point_vector.applyMatrix4(projScreenMat);
 
-    //console.log(renderer.getSize());
-
     return { x: ( point_vector.x + 1 ) * renderer.getSize().width / 2,
         y: renderer.getSize().height - ( - point_vector.y + 1) * renderer.getSize().height / 2 };
 }
 
 function selectPoints()
 {
-    var currentMouse = {},
-    mouseDownCorrected = {},
-    units,
-    bounds,
-    inside = false,
-    selectedUnits = [],
-    dupeCheck = {},
-    mouseupcoords = {};
-    
+    var currentMouse = {};
+    var mouseDownCorrected = {};
+    var units;
+    var bounds;
+    var inside = false;
+    var selectedUnits = [];
+    var dupeCheck = {};
+    var mouseUpCoords = {};
     var xypos;
 
     //currentMouse.x = location.x;
     //currentMouse.y = location.y;
 
-    mouseDownCorrected.x = mousedowncoords.x;
-    mouseDownCorrected.y = renderer.getSize().height - mousedowncoords.y;
+    mouseDownCorrected.x = mouseDownCoords.x;
+    mouseDownCorrected.y = renderer.getSize().height - mouseDownCoords.y;
 
-    mouseupcoords.x = mRelPos.x + mouseDownCorrected.x;
-    mouseupcoords.y = -mRelPos.y + mouseDownCorrected.y;
+    mouseUpCoords.x = mRelPos.x + mouseDownCorrected.x;
+    mouseUpCoords.y = -mRelPos.y + mouseDownCorrected.y;
 
-    bounds = findBounds(mouseupcoords, mouseDownCorrected);
+    bounds = findBounds(mouseUpCoords, mouseDownCorrected);
     
     var colors = points.geometry.getAttribute("color").array;
     var positions = points.geometry.getAttribute("position").array;    
@@ -211,12 +203,10 @@ function selectPoints()
         p.y = positions[i + 1];
         p.z = positions[i + 2];
         xypos = toScreenXY(p);
-        //console.log(xypos)
+
         inside = withinBounds(xypos, bounds);
-        //console.log(inside);
         if (inside)
         {
-            //var color = new THREE.Color();
             //color.setRGB(0.7, 0.0, 0.0);
             colors[ i ]     = 1.0;
             colors[ i + 1 ] = 0.4;
@@ -234,13 +224,11 @@ function onMouseDown( event )
     //event.preventDefault();
     //if (controls.shiftDown === true) return;
 
-    mousedown = true;
-    mousedowncoords = {};
+    mouseDown = true;
+    mouseDownCoords = {};
 
-    mousedowncoords.x = event.clientX;
-    mousedowncoords.y = event.clientY;
-    //console.log("Mouse down:");
-    //console.log(mousedowncoords);
+    mouseDownCoords.x = event.clientX;
+    mouseDownCoords.y = event.clientY;
 }
 
 function onMouseMove( event )
@@ -249,13 +237,10 @@ function onMouseMove( event )
     event.stopPropagation();
 
     // make sure we are in a select mode.
-    if(mousedown){
+    if(mouseDown){
         marquee.fadeIn();
-        mRelPos.x = event.clientX - mousedowncoords.x;
-        mRelPos.y = event.clientY - mousedowncoords.y;
-
-        ////console.log("Mouse move");
-        ////console.log(mRelPos);
+        mRelPos.x = event.clientX - mouseDownCoords.x;
+        mRelPos.y = event.clientY - mouseDownCoords.y;
 
         // square variations
         // (0,0) origin is the TOP LEFT pixel of the canvas.
@@ -265,17 +250,30 @@ function onMouseMove( event )
         //  4 | 3
         // there are 4 ways a square can be gestured onto the screen.  the following detects these four variations
         // and creates/updates the CSS to draw the square on the screen
-        if (mRelPos.x < 0 && mRelPos.y < 0) {
-          marquee.css({left: event.clientX + 'px', width: -mRelPos.x + 'px', top: event.clientY + 'px', height: -mRelPos.y + 'px'});
+        if (mRelPos.x < 0 && mRelPos.y < 0)
+        {
+          marquee.css({left: event.clientX + 'px',
+                       width: -mRelPos.x + 'px',
+                       top: event.clientY + 'px',
+                       height: -mRelPos.y + 'px'});
         } else if ( mRelPos.x >= 0 && mRelPos.y <= 0)
         {
-          marquee.css({left: mousedowncoords.x + 'px',width: mRelPos.x + 'px', top: event.clientY, height: -mRelPos.y + 'px'});
+          marquee.css({left: mouseDownCoords.x + 'px',
+                       width: mRelPos.x + 'px',
+                       top: event.clientY,
+                       height: -mRelPos.y + 'px'});
         } else if (mRelPos.x >= 0 && mRelPos.y >= 0)
         {
-          marquee.css({left: mousedowncoords.x + 'px', width: mRelPos.x + 'px', height: mRelPos.y + 'px', top: mousedowncoords.y + 'px'});
+          marquee.css({left: mouseDownCoords.x + 'px',
+                       width: mRelPos.x + 'px',
+                       height: mRelPos.y + 'px',
+                       top: mouseDownCoords.y + 'px'});
         } else if (mRelPos.x < 0 && mRelPos.y >= 0)
         {
-          marquee.css({left: event.clientX + 'px', width: -mRelPos.x + 'px', height: mRelPos.y + 'px', top: mousedowncoords.y + 'px'});
+          marquee.css({left: event.clientX + 'px',
+                       width: -mRelPos.x + 'px',
+                       height: mRelPos.y + 'px',
+                       top: mouseDownCoords.y + 'px'});
         }
     }
 }
@@ -285,13 +283,11 @@ function onMouseUp( event )
     //event.preventDefault();
     event.stopPropagation();
     //if (controls.shiftDown === true) return;
-    //console.log("Mouse up");
 
-        // reset the marquee selection
+    // reset the marquee selection
     selectPoints();
     resetMarquee();
 }
-
 
 function onWindowResize()
 {
@@ -308,7 +304,6 @@ function setUpGUI() {
     effectController = {
         input: "",
         output: ""
-        
     };
 
     var gui = new dat.GUI();
