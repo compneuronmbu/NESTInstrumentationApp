eee3ff78fd360efa7489f1d87b732a5cdb2b809f
Håkon Mørk <hakon.mork@nmbu.no>
2017-09-29 12:20:10 +0000

    Added missing library file

--- nett_modules/exports.sh
+++ nett_modules/exports.sh
@@ -3,4 +3,4 @@
 # run with '. ./nett_modules/exports.sh'
 # then add nest with 'export PYTHONPATH=$PYTHONPATH:path-to-pynest'
 export PYTHONPATH=$PWD/nett_modules/python_modules/nett-python:$PWD/nett_modules/python_modules/protobuf
-export LD_LIBRARY_PATH=$PWD/nett_modules/lib/boost/threading-multi
+export LD_LIBRARY_PATH=$PWD/nett_modules/lib/boost/threading-multi:$PWD/nett_modules/python_modules/zeromq
