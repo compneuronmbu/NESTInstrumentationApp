6b64228fc6318c9b1694135845f406c2ea95fa40
Håkon Mørk <hakon.mork@nmbu.no>
2017-10-11 16:14:46 +0200

    Using SSL again

--- NESTConnectionApp_server.py
+++ NESTConnectionApp_server.py
@@ -268,6 +268,6 @@ def simulationData():
 if __name__ == '__main__':
     # app.run()
 
-    # socketio.run(app, host="", port=7000, log_output=True, keyfile='/home/ubuntu/certs/fsd-cloud42_zam_kfa-juelich_de.key', certfile='/home/ubuntu/certs/fsd-cloud42_zam_kfa-juelich_de.pem')
-    socketio.run(app, host="", port=7000, log_output=True)
+    socketio.run(app, host="", port=7000, log_output=True, keyfile='/home/ubuntu/certs/fsd-cloud42_zam_kfa-juelich_de.key', certfile='/home/ubuntu/certs/fsd-cloud42_zam_kfa-juelich_de.pem')
+    # socketio.run(app, host="", port=7000, log_output=True)
     # server.serve_forever()
